a8a050f8b5051f82115e6c1ca292f52896b6d394
Gyulavári Peti <gypeti23@gmail.com>
2020-06-08 21:44:59 +0200

    feat: add LEAVE_ROOM action to vuex

--- src/store/module-room/actions.js
+++ src/store/module-room/actions.js
@@ -1,8 +1,16 @@
-import { socket } from 'boot/socketio'
+import { socket } from "boot/socketio";
 
 export function create({}, payload) {
   try {
-    socket.emit("JOIN_ROOM",payload);
+    socket.emit("JOIN_ROOM", payload);
+  } catch (error) {
+    return false;
+  }
+}
+
+export function patch({}, payload) {
+  try {
+    socket.emit("LEAVE_ROOM", payload);
   } catch (error) {
     return false;
   }
