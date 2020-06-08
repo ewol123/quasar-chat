1b20643e9ed0e0a56ab6631aa4d13bfaf4007a6b
Gyulavári Peti <gypeti23@gmail.com>
2020-06-08 21:43:51 +0200

    lint: vuex user-module actions

--- src/store/module-user/actions.js
+++ src/store/module-user/actions.js
@@ -1,8 +1,8 @@
-import { socket } from 'boot/socketio'
+import { socket } from "boot/socketio";
 
 export function create({}, payload) {
   try {
-    socket.emit("CREATE_USER",payload);
+    socket.emit("CREATE_USER", payload);
   } catch (error) {
     return false;
   }
