a8909c5f29d68a17ce7c61d2e00c784067debdd3
Gyulavári Peti <gypeti23@gmail.com>
2020-06-11 18:48:19 +0200

    feat: add messages and users getter to room vuex module

--- src/store/module-room/getters.js
+++ src/store/module-room/getters.js
@@ -4,3 +4,9 @@ export function isInitialized(state) {
 export function loading(state) {
   return state.loading;
 }
+export function messages(state) {
+  return state.messages;
+}
+export function users(state) {
+  return state.users;
+}
