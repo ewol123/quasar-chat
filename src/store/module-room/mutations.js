52c92472d424ff7939f2fa71e929cfbf00902347
Gyulavári Peti <gypeti23@gmail.com>
2020-06-11 18:50:50 +0200

    fix: room set mutation

--- src/store/module-room/mutations.js
+++ src/store/module-room/mutations.js
@@ -1,9 +1,14 @@
 export function set(state, payload) {
-  state.loading = false;
   Object.keys(payload).forEach(key => {
-    if (!state[key] || !Array.isArray(state[key])) Object.assign(state, { [key]: payload[key] })
-    else Object.assign(state, { [key]: [...state[key], payload[key]] })
+    if (!state[key] || !Array.isArray(state[key]))
+      Object.assign(state, { [key]: payload[key] });
+    else {
+      if (Array.isArray(payload[key]))
+        Object.assign(state, { [key]: payload[key] });
+      else Object.assign(state, { [key]: [...state[key], payload[key]] });
+    }
   });
+  state.loading = false;
 }
 
 export function loading(state, payload) {
