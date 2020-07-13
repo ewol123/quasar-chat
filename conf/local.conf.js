203e267f8f10a0d34f7398e87e747b8ccf461c30
Gyulavári Peti <gypeti23@gmail.com>
2020-07-13 22:10:36 +0200

    fix: testing

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -1,58 +1,29 @@
 nightwatch_config = {
   src_folders: ["tests/local"],
-
   selenium: {
     start_process: false,
     host: "hub-cloud.browserstack.com",
     port: 80
   },
 
-  common_capabilities: {
-    build: "nightwatch-browserstack",
-    "browserstack.user": "petigyulavri1",
-    "browserstack.key": "yCTvsE2M4cCQ8EUpikpY",
-    "browserstack.debug": true,
-    "browserstack.local": true
-  },
-
   test_settings: {
-    default: {},
-    chrome: {
+    default: {
       desiredCapabilities: {
-        browser: "chrome"
-      }
-    },
-    firefox: {
-      desiredCapabilities: {
-        browser: "firefox"
-      }
-    },
-    safari: {
-      desiredCapabilities: {
-        browser: "safari"
-      }
-    },
-    ie: {
-      desiredCapabilities: {
-        browser: "internet explorer"
+        "browserstack.user": process.env.BROWSERSTACK_USERNAME,
+        "browserstack.key": process.env.BROWSERSTACK_ACCESS_KEY,
+        "browserstack.local": true,
+        browser: "chrome",
+        name: "Bstack-[Nightwatch] Local Test"
       }
     }
   }
 };
 
-// Code to support common capabilites
+// Code to copy seleniumhost/port into test settings
 for (var i in nightwatch_config.test_settings) {
   var config = nightwatch_config.test_settings[i];
   config["selenium_host"] = nightwatch_config.selenium.host;
   config["selenium_port"] = nightwatch_config.selenium.port;
-  config["desiredCapabilities"] = config["desiredCapabilities"] || {};
-  for (var j in nightwatch_config.common_capabilities) {
-    config["desiredCapabilities"][j] =
-      config["desiredCapabilities"][j] ||
-      nightwatch_config.common_capabilities[j];
-  }
 }
-
-console.log(JSON.stringify(nightwatch_config));
 
 module.exports = nightwatch_config;
