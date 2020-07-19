678d5308dc62afd547b033e9b6f3e48580f7357e
Gyulavári Peti <gypeti23@gmail.com>
2020-07-19 22:49:24 +0200

    fix: browserstack port env var

--- tests/local/index_test.js
+++ tests/local/index_test.js
@@ -1,7 +1,7 @@
 module.exports = {
   "index test": function(browser) {
     browser
-      .url("http://localhost:8081/")
+      .url(`http://localhost:${process.env.BROWSERSTACK_PORT || 8081}/`)
       .waitForElementVisible("body", 1000)
       .assert.containsText("body", "Welcome, please enter your user details")
       .assert.visible("input[aria-label=Name]")
