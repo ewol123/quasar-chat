e84c297b76e56084e0ae4ede4b31044352b9e097
Gyulavári Peti <gypeti23@gmail.com>
2020-07-20 18:39:02 +0200

    test: sample tests

--- tests/local/index_test.js
+++ tests/local/index_test.js
@@ -8,9 +8,10 @@ module.exports = {
       .setValue("input[aria-label=Name]", "browserstack")
       .assert.visible("#go-btn")
       .click("#go-btn")
-      .assert.visible("#kb-arrow-left")
-      .click("#kb-arrow-left")
-      .assert.containsText("aside", "No active users")
+      .assert.containsText("#main-text1", "No active users")
+      .assert.visible("#main-btn-video")
+      .click("#main-btn-video")
+      .assert.containsText("#index-text1", "Type in your room number and click")
       .end();
   }
 };
